079067d6140085a8416af26e0737731726290345
NEEB <avijramoutar@gmail.com>
2024-12-06 00:48:21 -0500

    Lol

---
+++ Main.py
@@ -0,0 +1,13 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+#print("Hello World")
+
+scope = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+
+#Warframe reference
+credits = ServiceAccountCredentials.from_json_keyfile_name(r"C:\Users\Avi\IdeaProjects\untitled\Definetly-Not-a-Key.json",scope)
+
+client = gspread.authorize(credits)
+
+sheet = client.open('TEST').sheet1
