d6d8384af90e67affd4ff0b297a9f89bd39cb38b
NEEB <avijramoutar@gmail.com>
2024-12-06 15:51:51 -0500

    Fix credentials loading and clean up API key usage

--- Main.py
+++ Main.py
@@ -1,14 +1,32 @@
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2.service_account import Credentials
 
-#print("Hello World")
 
-scope = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+
+#Just read the name dawg it aint complicated
+def load_credentials_from_file(file_path):
+    try:
+        creds = Credentials.from_service_account_file(file_path, scopes=scope)
+        return creds
+    except Exception as e:
+        raise ValueError(f"Failed to load credentials from {file_path}: {e}")
+
+
+scope = ['https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file",
+         "https://www.googleapis.com/auth/drive"]
 
 #Warframe reference
-credits = ServiceAccountCredentials.from_json_keyfile_name(r"C:\Users\Avi\IdeaProjects\untitled\Definetly-Not-a-Key.json",scope)
+#CHANGE FILE PATH HERE!!!!!
+credits = load_credentials_from_file(r"Your\File\Path\Here")
 
 client = gspread.authorize(credits)
 
 
 sheet = client.open('TEST').sheet1
+
+#Accesor Methods
+
+#print(sheet.get_all_records())
+#print(sheet.row_values(4))
+#print(sheet.cell(3,3).value)
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Not Writing All that
+Line 22 of Main make sure to replace with file path of your own api keys
+:)
