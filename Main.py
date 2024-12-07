32dae4d13e0c0866e629057a773ddde5386bf5be
NEEB <avijramoutar@gmail.com>
2024-12-06 19:04:35 -0500

    Your commit message here

--- Main.py
+++ Main.py
@@ -2,8 +2,6 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 
-
-#Just read the name dawg it aint complicated
 def load_credentials_from_file(file_path):
     try:
         creds = Credentials.from_service_account_file(file_path, scopes=scope)
@@ -16,17 +14,32 @@ scope = ['https://www.googleapis.com/auth/spreadsheets',
          "https://www.googleapis.com/auth/drive.file",
          "https://www.googleapis.com/auth/drive"]
 
-#Warframe reference
+
 #CHANGE FILE PATH HERE!!!!!
 credits = load_credentials_from_file(r"Your\File\Path\Here")
-
 client = gspread.authorize(credits)
 
-
+#Replace 'TEST' with Sheet name
 sheet = client.open('TEST').sheet1
 
-#Accesor Methods
 
-#print(sheet.get_all_records())
-#print(sheet.row_values(4))
-#print(sheet.cell(3,3).value)
+#Accesor Methods use within scope of sheet
+
+print(sheet.get_all_records())
+print(sheet.row_values(4))
+print(sheet.col_values(3))
+print(sheet.cell(3,3).value)
+
+#Editor Methods
+sheet.insert_row(["TestText",56,"Hello",True],5)
+sheet.delete_rows(5)
+#sheet.update_cell(3,1,"TEST")
+
+"""
+Finds first appreance of given text and stores it's value/location
+"""
+#search = sheet.find("TestText")
+#print(search.value) returns "TestText"
+#print(search.row)
+#print(search.col)
+
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-Line 22 of Main make sure to replace with file path of your own api keys
-:)
+To set up a Google Cloud Project with a Service Account for accessing the Google Sheets API,
+start by creating a project in the Google Cloud Console. Once the project is created, enable the necessary APIs—specifically, 
+the Google Sheets API and Google Drive API—through the "APIs & Services > Library" section. 
+Next, create a Service Account by navigating to "IAM & Admin > Service Accounts," giving it a descriptive name, and assigning it an appropriate role, such as Editor or a more restrictive role like Sheets Admin or Drive API User. 
+After creating the Service Account, generate a JSON key by selecting "Add Key > Create New Key" from the Service Account's details page and choosing the JSON format. 
+Download and securely store this key, as it contains credentials for accessing your project.
+
+IMPORTANT!!
+To allow the Service Account to access specific Google Sheets, share the desired spreadsheet with the Service Account email, found in the JSON file (usually in the format @<project-id>.iam.gserviceaccount.com), granting appropriate permissions such as Viewer or Editor
+Line 22 of Main make sure to replace with file path of your own api keys :)
